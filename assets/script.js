1807961fbc476ce798cd0e3d351760b0ad878404
asif-96 <shuyeb.dk@gmail.com>
2021-09-23 23:43:04 +0600

    working on js file

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,7 @@ const displayProducts = (display_items) => {
     display_items.map(item => {
         const itemImage = item.image;
         const itemName = item.title;
+        const productId = item.id;
         // create product div
         const productDiv = document.createElement("div");
         productDiv.classList.add("product_div");
@@ -30,12 +31,19 @@ const displayProducts = (display_items) => {
             <img class="item-img" src="${itemImage}"/>
             <h4 style="margin: 10px 0px; items-align : center">${itemName}</h4>
             <h5 style="margin-bottom: 10px; items-align : center">${item.category}</h5>
-            <h2 style="items-align : center">$${item.price}</h2>
+            <div class="sub-div">
+                <h2 style="items-align : center">$${item.price}</h2>
+                <button class="btn" onclick="addItem(${productId})">Add To <i class="ri-shopping-cart-2-line"></i></button>
+            </div>
         `;
         productDiv.innerHTML = productDetails;
         itemDiv.appendChild(productDiv);
     })
 }
 
+
+function addItem(id){
+    console.log("Item clicked"+id);
+}
 
 
